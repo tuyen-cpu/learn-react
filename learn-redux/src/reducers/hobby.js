a293b015c70ca3c2a894bc7318aecd6f734a0411
tuyen-cpu <tranquangtuyen326326@gmail.com>
2022-02-10 23:30:17 +0700

    init proj, setup redux store

---
+++ learn-redux/src/reducers/hobby.js
@@ -0,0 +1,13 @@
+const hobbyReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_HOBBY": {
+      break;
+    }
+    case "SET_ACTIVE_HOBBY": {
+      break;
+    }
+    default:
+      return state;
+  }
+};
+export default hobbyReducer;
